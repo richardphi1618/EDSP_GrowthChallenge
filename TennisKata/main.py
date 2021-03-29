da0d1138be8f110098f6efe9f9c58fcdf6505c33
richphi <richardphi1618@gmail.com>
2021-03-29 19:58:22 -0400

    condensed a bunch

--- TennisKata/main.py
+++ TennisKata/main.py
@@ -2,92 +2,85 @@ from tkinter import *
 root = Tk()
 
 #variable is stored in the root object
-root.counter1 = 0
-root.counter2 = 0
-root.score1 = "Love"
-root.score2 = "Love"
+root.counter = [0]*3
+root.score = [0]*3
+
+SCORE = {
+    0: "Love",
+    1: "Fifteen",
+    2: "Thirty",
+    3: "Forty",
+}
 
 def input1():
-    root.counter1 += 1
-    print(root.counter1)
+    root.counter[1] += 1
+    print(f"Player1: {root.counter[1]}")
     TennisScore()
     UpdateScore()
     
 def input2():
-    root.counter2 += 1
-    print(root.counter2)
+    root.counter[2] += 1
+    print(f"Player2: {root.counter[2]}")
     TennisScore()
     UpdateScore()
 
 def input3():
-    root.counter1 = 0
-    root.counter2 = 0
+    root.counter[1] = 0
+    root.counter[2] = 0
     print('reset')
     TennisScore()
     UpdateScore()
 
 def UpdateScore():
-    L1['text'] = str(root.score1)
-    L2['text'] = str(root.score2)
+    L1['text'] = str(root.score[1])
+    L2['text'] = str(root.score[2])
 
 def TennisScore():
     
-    if (root.counter1 == 0):
-        root.score1 = "Love"
-    if (root.counter2 == 0):
-        root.score2 = "Love"
+    for i in range(3):
+        if root.counter[i] <= 3 :
+            root.score[i] = SCORE[root.counter[i]]
 
-    if (root.counter1 == 1):
-        root.score1 = "15"
-    if (root.counter2 == 1):
-        root.score2 = "15"
+    if (root.counter[1] == root.counter[2] and root.counter[1] > 0 and root.counter[2] > 0) :
+        root.score[1] = f"{root.score[1]}-ALL"
+        root.score[2] = f"{root.score[2]}-ALL"
     
-    if (root.counter1 == 2):
-        root.score1 = "30"
-    if (root.counter2 == 2):
-        root.score2 = "30"
+    if (root.counter[1] == root.counter[2] and root.counter[1] >= 3) :
+        root.score[1] = "Deuce"
+        root.score[2] = "Deuce"
     
-    if (root.counter1 == 3):
-        root.score1 = "40"
-    if (root.counter2 == 3):
-        root.score2 = "40"
+    if (root.counter[1] >= 3 and root.counter[2] >= 3 and root.counter[1] > root.counter[2]):
+        root.score[1] = "Advantage"
+        root.score[2] = "-"
+    if(root.counter[1] >= 3 and root.counter[2] >= 3 and root.counter[1] < root.counter[2]):
+        root.score[1] = "-"
+        root.score[2] = "Advantage"
 
-    if (root.counter1 == root.counter2 and root.counter1 > 0 and root.counter2 > 0) :
-        root.score1 = f"{root.score1}-ALL"
-        root.score2 = f"{root.score2}-ALL"
-    
-    if (root.counter1 == root.counter2 and root.counter1 >= 3) :
-        root.score1 = "Deuce"
-        root.score2 = "Deuce"
-    
-    if (root.counter1 >= 3 and root.counter2 >= 3 and root.counter1 > root.counter2):
-        root.score1 = "Advantage"
-        root.score2 = "-"
-    if(root.counter1 >= 3 and root.counter2 >= 3 and root.counter1 < root.counter2):
-        root.score1 = "-"
-        root.score2 = "Advantage"
-
-    if (root.counter1 >= 4 and root.counter1 > root.counter2+1):
-        root.score1 = "winner"
-        root.score2 = "loser"
-    if(root.counter2 >= 4 and root.counter1+1 < root.counter2):
-        root.score1 = "loser"
-        root.score2 = "winner"      
+    if (root.counter[1] >= 4 and root.counter[1] > root.counter[2]+1):
+        root.score[1] = "winner"
+        root.score[2] = "loser"
+    if (root.counter[2] >= 4 and root.counter[1]+1 < root.counter[2]):
+        root.score[1] = "loser"
+        root.score[2] = "winner"      
 
 
 if __name__ == "__main__":
-    L1 = Label(root.counter1, text="Love")
-    L1.pack()
-    b1 = Button(root, text="Point Player 1", command=input1)
-    b1.pack()
 
-    L2 = Label(root.counter2, text="Love")
-    L2.pack()
+    root.geometry("265x100")
+    root.title("Tennis ScoreCard")
+    
+    L1 = Label(root.score[1], text="Love")
+    L1.grid(row=1, column=1, padx=10, pady=5)
+    b1 = Button(root, text="Point Player 1", command=input1)
+    b1.grid(row=2, column=1, padx=10, pady=5)
+
+    L2 = Label(root.score[2], text="Love")
+    L2.grid(row=1, column=2, padx=10, pady=5)
     b2 = Button(root, text="Point Player 2", command=input2)
-    b2.pack()
+    b2.grid(row=2, column=2, padx=10, pady=5)
 
     b3 = Button(root, text="reset", command=input3)
-    b3.pack()
+    b3.grid(row=1, column=3, padx=10, pady=10)
 
     TennisScore()
     UpdateScore()
