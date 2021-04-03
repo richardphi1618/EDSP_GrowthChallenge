2837005f58f82b3209a2b77516842fcbdabd94a0
richphi <richardphi1618@gmail.com>
2021-04-03 12:11:52 -0400

    working on adding img updates

--- YahtzeeKata/main.py
+++ YahtzeeKata/main.py
@@ -7,13 +7,16 @@ while True:         #Main Dice Rolling Loop
     sg.theme('dark grey 9')
     
     die = [6]*6
+    dice_img = []
+
+    for i in range(len(die)):
+        dice_img += [sg.Image(filename=f'./images/{i+1}.png', size=(250,250), key= f'_DieImage{i}_')]
 
     layout = [
-    [sg.Column([dice])],
-    [sg.Image(filename=r'./images/{}.png'.format(die[col]), size=(250,250)) for col in range(6)],
-    [sg.Text('Number of Dice:')],
-    [sg.Button('Roll Dice', bind_return_key=True)],
+    [sg.Column([dice_img])],
+    [sg.Text('Controls:')],
     [sg.Button(f'Hold Die {col+1}') for col in range(6)],
+    [sg.Button('Roll Dice', bind_return_key=True)],
     [sg.Text('   ',size=(3,1), key='_TOTAL_', font='ANY 20', text_color = 'red')],]
 
     if previous_total != total:
@@ -28,3 +31,15 @@ while True:         #Main Dice Rolling Loop
             exit()
         else:
             print(event)
+
+        total = 0
+        if event == "Roll Dice":
+            for i in range(len(die)):           # for each die rolled, get the random # and build list of images
+                val = randint(1,6)
+                die[i] = val
+                dice_img += [sg.Image(filename=f'./images/{die[i]}.png', size=(250,250), key= f'_DieImage{i}_')]
+                total += die[i]
+            
+            for i in range(len(die)):
+                window.Element(f'_DieImage{i}_').Update(dice_img[i])
+            break
