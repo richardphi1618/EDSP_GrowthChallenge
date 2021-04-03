1f1cf97490164f67010fd925402ddcb4dcfcd971
richphi <richardphi1618@gmail.com>
2021-04-03 11:14:49 -0400

    restructure gui

--- YahtzeeKata/main.py
+++ YahtzeeKata/main.py
@@ -1,31 +1,30 @@
 import PySimpleGUI as sg
 from random import randint
 window, previous_total, dice, total = None, -1, [] , 0
+
 while True:         #Main Dice Rolling Loop
-    layout = [[sg.Column([dice])],
-                [sg.Text('Number of Dice:')],
-                [sg.In(size=(3,1), key='_NUM_DICE_')],
-                [sg.Button('Roll Dice', bind_return_key=True)],
-                [sg.Text('Total:', font='ANY 20', text_color='red')],
-                [sg.Text('   ',size=(3,1), key='_TOTAL_', font='ANY 20', text_color = 'red')],]
+
+    sg.theme('dark grey 9')
+    
+    die = [6]*6
+
+    layout = [
+    [sg.Column([dice])],
+    [sg.Image(filename=r'./images/{}.png'.format(die[col]), size=(250,250)) for col in range(6)],
+    [sg.Text('Number of Dice:')],
+    [sg.Button('Roll Dice', bind_return_key=True)],
+    [sg.Button(f'Hold Die {col+1}') for col in range(6)],
+    [sg.Text('   ',size=(3,1), key='_TOTAL_', font='ANY 20', text_color = 'red')],]
+
     if previous_total != total:
         window.Close() if window is not None else None
         window = sg.Window('Dice Roller', layout).Finalize()
     window.Element('_TOTAL_').Update(total)
+
     while True:                             # Loop to read # dice to roll, create list of images
         event, values = window.Read()
+
         if event is None:
             exit()
-        try:
-            num_dice = int(values['_NUM_DICE_'])
-        except:
-            continue
-        if  8 < num_dice < 0:
-            continue
-        previouis_dice = dice
-        dice, total = [], 0
-        for i in range(num_dice):           # for each die rolled, get the random # and build list of images
-            die = randint(1,6)
-            dice += [sg.Image(filename=r'./images/{}.png'.format(die), size=(250,250))]
-            total += die
-        break
+        else:
+            print(event)
